7650dcaa3ca9d5b782033d30943a9d1b12816d14
GentaShibasaki <genta.shibasaki.78@gmail.com>
2020-02-26 18:17:55 +0900

    fixed bug

--- Demo_Calendar/settings.py
+++ Demo_Calendar/settings.py
@@ -39,8 +39,6 @@ DATABASES = {
         'PORT': '',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
 
 try:
     from .local_settings import *
@@ -52,6 +50,10 @@ if not DEBUG:
     import django_heroku
     django_heroku.settings(locals())
 
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 ALLOWED_HOSTS = ['*']
 
 
